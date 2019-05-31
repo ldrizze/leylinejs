13a260aeccabb06f9ef939c833ff64e2bd170695
Luis Fedrizze <luisfedrizze@gmail.com>
2019-05-31 16:30:02 -0300

    close connection with user identification instance

--- app/classes/Driver.ts
+++ app/classes/Driver.ts
@@ -1,9 +1,10 @@
 import { Payload } from './Payload'
+import { User } from './User'
 
 export abstract class Driver {
   constructor (protected onConnectFn: Function, protected onReceiveFn: Function, protected onCloseFn: Function) {}
   public abstract initialize (): void
   public abstract listen (port: number): void
   public abstract send (payload: Payload): void
-  public abstract close (socket: any): void
+  public abstract close (user: User): void
 }
