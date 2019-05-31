6af374d82b7e6fa8be66fa838390ba62fdf51c16
Luis Fedrizze <luisfedrizze@gmail.com>
2019-05-31 16:32:56 -0300

    user identification on constructor

--- app/classes/User.ts
+++ app/classes/User.ts
@@ -1,6 +1,6 @@
 export abstract class User {
 
-  protected _identification: string
+  constructor (protected _identification: string) {}
 
   get identification (): string {
     return this._identification
