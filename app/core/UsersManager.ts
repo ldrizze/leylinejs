0dc2a769947e3b0ff92c7033b3ab77a427d0607f
Luis Fedrizze <luisfedrizze@gmail.com>
2019-06-02 02:11:28 -0300

    users manager implementation

---
+++ app/core/UsersManager.ts
@@ -0,0 +1,33 @@
+import { Collection, User } from '../classes'
+import * as uuid from 'uuid'
+
+export class UsersManager {
+
+  /**
+   * List of active users on server
+   */
+  private static users: Collection<User> = new Collection<User>('identification')
+
+  /**
+   * Create new User
+   */
+  public static createUser () {
+    return new User(uuid())
+  }
+
+  /**
+   * Remove user from active users list
+   * @param identification User identification
+   */
+  public static removeUser (identification: string) {
+    return this.users.remove(identification)
+  }
+
+  /**
+   * Get an user from active users list
+   * @param identification User identification
+   */
+  public static getUser (identification: string): User {
+    return this.users.find(identification)
+  }
+}
