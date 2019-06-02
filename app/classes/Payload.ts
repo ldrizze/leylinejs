2380c31f0b55d640d1a65cec5fdfa4641b85f5e4
Luis Fedrizze <luisfedrizze@gmail.com>
2019-06-02 02:10:10 -0300

    remove abstraction from payload class and add user instance reference

--- app/classes/Payload.ts
+++ app/classes/Payload.ts
@@ -4,19 +4,18 @@ import { User } from './User'
  * Base class to compose user's payload
  * to be interpreted by system
  */
-export abstract class Payload {
-
-  /**
-   * User instance from main user collections
-   */
-  public user: User
+export class Payload {
 
   /**
    * Compose payload based on user identification and the data received from driver
-   * @param userIdentification User identification provided by driver
+   * @param user User instance from main user collections
    * @param data Data received from respective driver
    */
-  constructor (protected userIdentification: string, protected data: string) {}
+  constructor (protected _user: User, public data: string) {}
+
+  get user () {
+    return this._user
+  }
 
   toString () {
     return this.data.toString()
